1cd0b1c6750bc5c1ad8ee17f33a5fbaf4e79330c
unixbob <unixbob@gmail.com>
2020-03-02 17:18:03 +0000

    Chapter 34

--- Section_2/app.js
+++ Section_2/app.js
@@ -1,24 +1,40 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
-
+    const method = req.method;
     if (url == '/')
     {
         res.write('<html>');
         res.write('<head>Enter a Message</head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
         res.write('</html>');
-        return res.end;  
+        return res.end();  
     }
-
-    console.log(req.url, req.method, req.headers)
+    if (url == '/message' && method == 'POST' ) {
+        const body = [];
+         req.on('data', (chunk) => {
+		console.log(chunk);
+            body.push(chunk);
+         });
+         req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+         });
+         
+         res.statusCode = 302;
+         res.setHeader('Location','/');
+         return res.end();
+    }
+    // console.log(req.url, req.method, req.headers)
     // process.exit;
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<body>Rob</body>')
     res.write('</html>');
-    res.end;
+    res.end();
 });
 
 server.listen(3000);
